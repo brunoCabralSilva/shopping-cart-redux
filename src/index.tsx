acc57f2fb2552777fbb1fc0bd91218dd753c8376
bruno <bruno.cabral.silva2018@gmail.com>
2023-07-10 21:15:17 -0300

    Troca de BrowserRouter para HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={ store }>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
